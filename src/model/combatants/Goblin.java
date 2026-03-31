d4a9b59513aa95e1c4804a403b2ca1feeb60cfba
JX <54003434+Jiexun-Ke@users.noreply.github.com>
2026-03-31 15:58:31 +0800

    Delete src/model/combatants directory

--- src/model/combatants/Goblin.java
+++
@@ -1,5 +0,0 @@
-public class Goblin {
-    public Goblin(){
-        super("Goblin", 55, 35, 15, 25);
-    }
-}
--- src/model/combatants/Wolf.java
+++
@@ -1,5 +0,0 @@
-public class Wolf extends Enemy{
-    public Wolf(){
-        super("Wolf", 40, 45, 5, 35);
-    }
-}
